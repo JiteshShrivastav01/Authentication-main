aef701bf301cbaf40a5d3d54eb74602ea37f8485
JiteshShrivastav01 <jitesh.cs01@gmail.com>
2023-07-09 04:08:17 +0530

    Login Page

---
+++ src/components/Auth/AuthForm.js
@@ -0,0 +1,127 @@
+import { useState, useRef , useEffect } from 'react';
+
+import classes from './AuthForm.module.css';
+
+const AuthForm = () => {
+  const enteredEmail=useRef()
+  const enteredPassword=useRef()
+  const [isLogin, setIsLogin] = useState(true);
+  const [passError,setPassError]=useState('')
+  const [isPassError,setIsPassError]=useState(false)
+  const [error,setError]=useState('')
+  const [isSubmit,setIsSubmit]=useState(false)
+
+  useEffect(() => {
+    if (isSubmit) {
+      const timer = setTimeout(() => {
+        setIsSubmit(false);
+      }, 1000); 
+
+      return () => clearTimeout(timer);
+    }
+
+    if (isPassError) {
+      const timer = setTimeout(() => {
+        setIsPassError(false);
+      }, 2000); 
+
+      return () => clearTimeout(timer);
+    }
+
+  }, [isPassError,isSubmit]);
+
+ 
+
+
+  const switchAuthModeHandler = () => {
+    setIsLogin((prevState) => !prevState);
+  };
+
+ const SubmitHandler=(e)=>{
+  e.preventDefault()
+  setIsSubmit(true)
+  setIsPassError(false)
+  const Email=enteredEmail.current.value
+  const Password=enteredPassword.current.value
+
+  if(Password.trim().length<6){
+    setIsPassError(true)
+    setPassError('Password length must be greater than 6')
+  }
+
+  if(isPassError){
+
+  }
+  else{
+    if(isLogin){
+       
+    }
+    else{
+      fetch('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyD0usF_SA_UnVC10SN__U6lRALDJWrihu8' ,{
+        method:'POST',
+        body:JSON.stringify({
+          email:Email,
+          password : Password,
+          returnSecureToken : true
+        }),
+        headers:{
+          'Content-type' : 'application/json'
+        }
+      }).then((res)=>{
+      if(res.ok){
+        alert('Submitted')
+      }
+      else{
+        return res.json().then(data=>{
+          setError('Authentication Error')
+          // if(data && data.error && data.error.message){
+          //   setError(data.error.message)
+          // }
+          alert(error)
+        })
+      }
+    })
+  }}
+  enteredEmail.current.value=''
+  enteredPassword.current.value=''
+
+ }
+
+
+  return (
+    <section className={classes.auth}>
+      <h1>{isLogin ? 'Login' : 'Sign Up'}</h1>
+      <form onSubmit={SubmitHandler}>
+        <div className={classes.control}>
+          <label htmlFor='email'>Your Email</label>
+          <input type='email' id='email' required ref={enteredEmail}/>
+        </div>
+        <div className={classes.control}>
+          <label htmlFor='password' >Your Password</label>
+          <input
+            type='password'
+            id='password'
+            ref={enteredPassword}
+            required
+          />
+          {isPassError && <small className={classes.passError}>{passError}</small>}
+        </div>
+        <div className={classes.actions}> 
+          {!isSubmit && <button type="submit">{isLogin ? 'Login' : 'Create Account'}</button>}
+          {isSubmit && <p>Sending Request ...</p>}
+        </div>
+        <div className={classes.actions}>
+          <button
+            type='button'
+            className={classes.toggle}
+            onClick={switchAuthModeHandler}
+          >
+            {isLogin ? 'Create new account' : 'Login with existing account'}
+          </button>
+        </div>
+      </form>
+    </section>
+  );
+};
+
+export default AuthForm
