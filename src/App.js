c515c9a274e9924945b5354bf7b44fc2cc82d186
JiteshShrivastav01 <jitesh.cs01@gmail.com>
2023-07-09 07:42:17 +0530

    task 20

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import { Switch, Route } from 'react-router-dom';
-
+import { Switch, Route, Redirect } from 'react-router-dom';
+import AuthContext from './Store/AuthContext';
 import Layout from './components/Layout/Layout';
 import UserProfile from './components/Profile/UserProfile';
 import AuthPage from './pages/AuthPage';
 import HomePage from './pages/HomePage';
+import { useContext } from 'react';
+
 
 function App() {
+
+  const ctx=useContext(AuthContext)
+
   return (
     <Layout>
       <Switch>
         <Route path='/' exact>
           <HomePage />
         </Route>
-        <Route path='/auth'>
+        {!ctx.isLoggedIn && <Route path='/auth'>
           <AuthPage />
-        </Route>
-        <Route path='/profile'>
+        </Route>}
+        {ctx.isLoggedIn && <Route path='/profile'>
           <UserProfile />
-        </Route>
+        </Route>}
+        {
+          <Route path='*'>
+             <Redirect path='/' />
+          </Route>
+        }
       </Switch>
     </Layout>
   );
